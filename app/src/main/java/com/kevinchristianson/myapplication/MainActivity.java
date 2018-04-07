40558e4cd7e070dca510fc8b7510f11164be5c77
Phillip Boldt <phillip.boldt@hotmail.com>
2018-04-07 13:26:32 -0500

    Added basic share page UI

--- app/src/main/java/com/kevinchristianson/myapplication/MainActivity.java
+++ app/src/main/java/com/kevinchristianson/myapplication/MainActivity.java
@@ -11,13 +11,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((TextView) findViewById(R.id.textView)).setText("Clicked");
-            }
-        });
+        setContentView(R.layout.share_page);
+//
+//        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                ((TextView) findViewById(R.id.textView)).setText("Clicked");
+//            }
+//        });
     }
 }
