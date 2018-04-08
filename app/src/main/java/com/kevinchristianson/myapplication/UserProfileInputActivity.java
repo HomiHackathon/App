8d21336cfa3f0f2d2d164c630c1ffce5475c7a31
Phillip Boldt <phillip.boldt@hotmail.com>
2018-04-07 22:59:45 -0500

    added UserProfileInput

---
+++ app/src/main/java/com/kevinchristianson/myapplication/UserProfileInputActivity.java
@@ -0,0 +1,13 @@
+package com.kevinchristianson.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfileInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_input);
+    }
+}
