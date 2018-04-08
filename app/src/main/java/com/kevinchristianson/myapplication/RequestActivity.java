daadf3ac1e016025464c6482ff1674b407487139
Grant <grant.lee.2017@gmail.com>
2018-04-08 00:49:12 -0500

    trial 1 request activity

--- app/src/main/java/com/kevinchristianson/myapplication/RequestActivity.java
+++ app/src/main/java/com/kevinchristianson/myapplication/RequestActivity.java
@@ -1,15 +1,20 @@
 package com.kevinchristianson.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.view.View.OnClickListener;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import butterknife.OnClick;
 
 public class RequestActivity extends AppCompatActivity {
 
@@ -20,7 +25,8 @@ public class RequestActivity extends AppCompatActivity {
     EditText textDaysBrrw;
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference mConditionRef = mRootRef.child("condition");
+    DatabaseReference requests = mRootRef.child("Request");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +34,28 @@ public class RequestActivity extends AppCompatActivity {
         setContentView(R.layout.request_page);
 
         //Get UI  elements
-        buttonborrow = (Button)findViewById(R.id.article_heading);
+        buttonborrow = (Button) findViewById(R.id.article_heading);
         textItemBrrw = (EditText) findViewById(R.id.editTextItmBrrw);
         textItemCtgry = (EditText) findViewById(R.id.editTextItemctgry);
         textRadius = (EditText) findViewById(R.id.editTextRadius);
         textDaysBrrw = (EditText) findViewById(R.id.editTextdaysbrrw);
 
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        mConditionRef.addValueEventListener(new ValueEventListener() {
+        buttonborrow.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String text = dataSnapshot.getValue(String.class);
-
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
+            public void onClick(View v) {
+                requests.setValue("hello world");
+                Intent intent = new Intent(RequestActivity.this, RequestActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
-    }
 
+//        @Override
+//        public void onClick(View view){
+//            String requestItem = textItemBrrw.getText().toString() + " ," + textItemCtgry.getText().toString() +
+//                    ", " + textRadius.getText().toString() + ", " + textDaysBrrw.getText().toString();
+//
+//        }
+    }
 }
