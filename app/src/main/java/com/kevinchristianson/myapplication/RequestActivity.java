afb05f400fa5a22db72e666d12ca7991e368bd2d
Grant <grant.lee.2017@gmail.com>
2018-04-08 04:54:11 -0500

    Connected following classes to database, attempting to reconfigure to support multiple entries

--- app/src/main/java/com/kevinchristianson/myapplication/RequestActivity.java
+++ app/src/main/java/com/kevinchristianson/myapplication/RequestActivity.java
@@ -1,6 +1,7 @@
 package com.kevinchristianson.myapplication;
 
 import android.content.Intent;
+import android.database.DatabaseErrorHandler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+import java.sql.DatabaseMetaData;
 
 import butterknife.OnClick;
 
@@ -24,19 +29,18 @@ public class RequestActivity extends AppCompatActivity {
     EditText textRadius;
     EditText textDaysBrrw;
 
-
-
-
+    DatabaseReference requests = FirebaseDatabase.getInstance().getReference().child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference().child("requests");
+    DatabaseReference itmBrrwdRef = mRootRef.child("Item Requested");
+    DatabaseReference itmCtgryRef = itmBrrwdRef.child("Item Category");
+    DatabaseReference radiusRef = mRootRef.child("Distance for Item");
+    DatabaseReference daysBrrwRef = mRootRef.child("Number of Days Needed to Borrow");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("request");
-
-        ref.setValue("Hello,World!!!!!!");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.request_page);
+
         //Get UI  elements
         buttonborrow = (Button) findViewById(R.id.article_heading);
         textItemBrrw = (EditText) findViewById(R.id.editTextItmBrrw);
@@ -44,22 +48,78 @@ public class RequestActivity extends AppCompatActivity {
         textRadius = (EditText) findViewById(R.id.editTextRadius);
         textDaysBrrw = (EditText) findViewById(R.id.editTextdaysbrrw);
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        itmBrrwdRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                textItemBrrw.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        itmCtgryRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                textItemCtgry.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        radiusRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                textRadius.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        daysBrrwRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                textDaysBrrw.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         buttonborrow.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //database.setValue("hello world");
+            public void onClick(View view) {
+                itmBrrwdRef.setValue(textItemBrrw.getText().toString());
+                itmCtgryRef.setValue(textItemCtgry.getText().toString());
+                radiusRef.setValue(textRadius.getText().toString());
+                daysBrrwRef.setValue(textDaysBrrw.getText().toString());
+
                 Intent intent = new Intent(RequestActivity.this, RequestActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
-
-
-//        @Override
-//        public void onClick(View view){
-//            String requestItem = textItemBrrw.getText().toString() + " ," + textItemCtgry.getText().toString() +
-//                    ", " + textRadius.getText().toString() + ", " + textDaysBrrw.getText().toString();
-//
-//        }
     }
 }
+
+
--- app/src/main/java/com/kevinchristianson/myapplication/ShareActivity.java
+++ app/src/main/java/com/kevinchristianson/myapplication/ShareActivity.java
@@ -29,7 +29,7 @@ public class ShareActivity extends AppCompatActivity {
         leftBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ShareActivity.this, MainActivity.class));
+                startActivity(new Intent(ShareActivity.this, RequestActivity.class));
                 finish();
             }
         });
