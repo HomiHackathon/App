b79080742596c3ca8b8643c366b8998e0fbcca46
Grant <grant.lee.2017@gmail.com>
2018-04-07 23:19:11 -0500

    updated request class

--- app/src/main/java/com/kevinchristianson/myapplication/RequestActivity.java
+++ app/src/main/java/com/kevinchristianson/myapplication/RequestActivity.java
@@ -1,11 +1,53 @@
 package com.kevinchristianson.myapplication;
 
-
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class RequestActivity {
 
     Button buttonborrow;
-    EditText
+    EditText textItemBrrw;
+    EditText textItemCtgry;
+    EditText textRadius;
+    EditText textDaysBrrw;
+
+    DatabaseReference mRootRef = FireBaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("condition");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.request_page);
+
+        //Get UI  elements
+        buttonborrow = (Button)findViewById(R.id.article_heading);
+        textItemBrrw = (EditText) findViewById(R.id.editTextItmBrrw);
+        textItemCtgry = (EditText) findViewById(R.id.editTextItemctgry);
+        textRadius = (EditText) findViewById(R.id.editTextRadius);
+        textDaysBrrw = (EditText) findViewById(R.id.editTextdaysbrrw);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
 }
