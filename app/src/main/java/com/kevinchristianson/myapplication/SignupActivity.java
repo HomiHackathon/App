de417d638939c1d3160b09edec4c364425c15924
Grant <grant.lee.2017@gmail.com>
2018-04-07 21:35:31 -0500

    Signup/Login Function Working

--- app/src/main/java/com/kevinchristianson/myapplication/SignupActivity.java
+++ app/src/main/java/com/kevinchristianson/myapplication/SignupActivity.java
@@ -38,19 +38,11 @@ public class SignupActivity extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
 
-        /*
-        btnResetPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SignupActivity.this, ResetPasswordActivity.class));
-            }
-        });
-        */
-
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
