f065f3f7c031c925d340a3a36c007e04be954579
Chiraag Gohel <cgohel@gmail.com>
2018-04-08 03:36:40 -0500

    Working feed

---
+++ app/src/main/java/com/kevinchristianson/myapplication/FeedActivity.java
@@ -0,0 +1,64 @@
+package com.kevinchristianson.myapplication;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+public class FeedActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private LinearLayoutManager linearLayoutManager;
+    private  List<String> adapterData;
+    private Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+        handler = new Handler();
+        linearLayoutManager = new LinearLayoutManager(FeedActivity.this);
+        // return the data object
+        adapterData = getFirstData();
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerViewAdapter = new RecyclerViewAdapter(FeedActivity.this, adapterData, recyclerView);
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerViewAdapter.setOnLoadMoreListener(new RecyclerViewAdapter.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                adapterData.add(null);
+                recyclerViewAdapter.notifyItemInserted(adapterData.size() - 1);
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapterData.remove(adapterData.size() - 1);
+                        recyclerViewAdapter.notifyItemRemoved(adapterData.size());
+                        for (int i = 0; i < 15; i++) {
+                            adapterData.add("Item" + (adapterData.size() + 1));
+                            recyclerViewAdapter.notifyItemInserted(adapterData.size());
+                        }
+                        recyclerViewAdapter.setLoaded();
+                    }
+                }, 2000);
+                System.out.println("load");
+            }
+        });
+    }
+    private List<String> getFirstData(){
+        List<String> listObject = new ArrayList<String>();
+        listObject.add("one");
+        listObject.add("one");
+        listObject.add("one");
+        listObject.add("one");
+        listObject.add("one");
+        listObject.add("one");
+        listObject.add("one");
+        listObject.add("one");
+        listObject.add("one");
+        listObject.add("one");
+        return listObject;
+    }
+}
